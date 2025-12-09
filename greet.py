2692d382570df795531211273c092be2ca446525
ajarpit02 <arpit.jain@techolution.com>
2025-12-09 19:22:44 +0530

    Create greet.py

---
+++ greet.py
@@ -0,0 +1,9 @@
+def say_hello(name):
+    return f"Hello, {name}!"
+
+def say_bye(name):
+    return f"Goodbye, {name}!"
+
+if __name__ == "__main__":
+    print(say_hello("Arpit"))
+    print(say_bye("Arpit"))
